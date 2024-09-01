7789d33ddc010081eff5baa29d47cf8c0c0e519f
AVMG <rijstenpap1234@hotmail.com>
2024-09-01 10:22:02 +0200

    updated UI

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,8 +1,8 @@
 import type {Metadata} from "next";
 
 export const metadata: Metadata = {
-    title: "Store",
-    description: "Browse all services available",
+    title: "CtrlPanel",
+    description: "New and improved billing panel for all your hosting needs.",
 };
 
 export default function RootLayout({
