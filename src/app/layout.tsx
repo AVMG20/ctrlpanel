c86aed29bafe6635f3fbd3f1bb76e219c80b111e
AVMG <rijstenpap1234@hotmail.com>
2024-09-04 22:45:31 +0200

    change theme back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
     return (
         <SessionWrapper>
-            <html lang="en" data-theme="soly">
+            <html lang="en" data-theme="ctrlpanel">
                 <body className={inter.className}>
                 {children}
                 </body>
