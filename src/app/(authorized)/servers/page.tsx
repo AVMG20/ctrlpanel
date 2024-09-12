5d5d8c002b0decb196eafbdef3df566736d806c0
Stefan Teunissen <stefanteunissen1@gmail.com>
2024-09-12 22:46:39 +0200

    Fix typo

--- src/app/(authorized)/servers/page.tsx
+++ src/app/(authorized)/servers/page.tsx
@@ -6,15 +6,15 @@ import Link from "next/link";
 export default async function Page() {
     return (
         <div>
-            <PageTitle title="Comming soon" description="This page is currently under construction. Please check back later."/>
+            <PageTitle title="Coming soon" description="This page is currently under construction. Please check back later."/>
             <Link href={'/store'}>Store</Link>
 
-            {/* Comming soon hero something fancy */}
+            {/* Coming soon hero something fancy */}
             <div className="flex items-center justify-center h-screen">
 
 
                 <div className="text-center">
-                    <h1 className="text-5xl font-bold text-gray-800">Comming Soon</h1>
+                    <h1 className="text-5xl font-bold text-gray-800">Coming Soon</h1>
                     <p className="text-xl text-gray-600">This page is currently under construction. Please check back later.</p>
                 </div>
             </div>
