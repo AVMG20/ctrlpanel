46d70f43e74e76f1bace49e5eab4b6bf2702b42e
AVMG <rijstenpap1234@hotmail.com>
2024-09-12 21:55:15 +0200

    update cache busting admin users

--- src/app/(authorized)/admin/users/page.tsx
+++ src/app/(authorized)/admin/users/page.tsx
@@ -1,6 +1,15 @@
 import PageTitle from "@/components/util/page-title";
 import {prisma} from "@/prisma";
 import UsersTable from "@/app/(authorized)/admin/users/table";
+import type {Metadata} from "next";
+
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 60 seconds.
+export const revalidate = 60;
+
+export const metadata: Metadata = {
+    title: "Admin | Users",
+};
 
 export default async function UsersPage() {
     const data = await prisma.user.findMany({
